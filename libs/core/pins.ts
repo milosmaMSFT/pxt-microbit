1e7be407f539abe03ae114c6f3f866625552ed1a
Peli de Halleux <jhalleux@microsoft.com>
2016-12-31 06:49:00 -0800

    adding ping function

--- libs/core/pins.ts
+++ libs/core/pins.ts
@@ -1,3 +1,12 @@
+enum PingUnit {
+    //% block="cm"
+    Centimeters,
+    //% block="μs"
+    MicroSeconds,
+    //% block="inches"
+    Inches
+}
+
 /**
  * Control currents in Pins for analog/digital signals, servos, i2c, ...
  */
@@ -37,6 +46,33 @@ namespace pins {
         let buf = createBuffer(pins.sizeOf(format))
         buf.setNumber(format, 0, value)
         pins.i2cWriteBuffer(address, buf)
+    }
+
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig the tigger pin
+     * @param echo the echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     */
+    //% advanced=true
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+        switch (unit) {
+            case PingUnit.Centimeters: return d / 58;
+            case PingUnit.Inches: return d / 148;
+            default: return d;
+        }
     }
 
     /**
